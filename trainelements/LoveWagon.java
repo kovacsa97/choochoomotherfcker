ea1aff8233ac5ed266380bf06237ca9d346042d3
szoke <szoke@152.66.183.9>
2017-04-14 00:18:30 +0200

    LoveWagon comment

--- trainelements/LoveWagon.java
+++ trainelements/LoveWagon.java
@@ -2,23 +2,29 @@ package trainelements;
 
 import color.Color;
 
+/**
+ *speciális vagon, melyen nõ az utasok boldogságpontja az idõ elteltével
+ */
 public class LoveWagon extends Wagon{
 
+	/**
+	 * @param weight
+	 * @param color
+	 * @param numPass
+	 * konstruktor
+	 */
 	public LoveWagon(int weight, Color color, int numPass) {
 		super(weight, color, numPass);
-		System.out.println("New LoveWagon created");
 		this.happinessOfPassengers = 0;
 	}
 	
+	/* (non-Javadoc)
+	 * @see trainelements.Wagon#handlePassengers()
+	 * növeli az utasok boldogságpontját
+	 */
 	@Override
 	public void handlePassengers(){
-		//System.out.println("HandlePassengers was called inside class LoveWagon");
 		this.happinessOfPassengers++;
-		//System.out.println("HandlePassengers returned");
 	}
 	
-	@Override
-	public String toString(){
-		return "LoveWagon";
-	}
 }
