f73e0e9b366dccfcfec189276d56debe9299282f
matesebi <matesebi96@gmail.com>
2017-04-13 20:40:06 +0200

    comments added from skeleton

--- position/Position.java
+++ position/Position.java
@@ -2,15 +2,34 @@ package position;
 
 import boardelements.BoardElement;
 
+/**
+ * Pozíció osztály mely tárol egy pályaelemet és azon belüli helyzetet
+ */
 public abstract class Position {
+	/**
+	 * A jelenlegi pályaelemen való poziíció
+	 */
 	protected int pos;
+	/**
+	 * A jelenlegi pályaelem
+	 */
 	protected BoardElement CurrentBE;
 	
+	/**
+	 * Konstruktor mely létrehoz egy pozíciót
+	 * @param cbe a pályaelem amin a pozíció keletkezik
+	 */
 	Position (BoardElement cbe){
 		//System.out.println("Position was created with parameters type of "+ cbe.toString());
 		CurrentBE = cbe;
 	}
 	
+	/**
+	 * Absztrakt mozgató fv, mely mozgatja a pozíciót egy megadott távolsággal
+	 * @param dist a távolság, melyel a pozíció mozog
+	 * @return a következõ, vagy jelenlegi pályaelem
+	 * @throws Exception
+	 */
 	public abstract void move(int dist) throws Exception;
 	
 	@Override
