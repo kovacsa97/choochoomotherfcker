988d309d83222f54aebdf0bacfd4a84479d3977a
szoke <szoke@152.66.183.9>
2017-04-14 00:49:21 +0200

    EntryPoint és Updateable comment

--- boardelements/EntryPoint.java
+++ boardelements/EntryPoint.java
@@ -14,22 +14,72 @@ import trainelements.Wagon;
 import update.Timer;
 import update.Updateable;
 
+/**
+ *Megadott idõközönként (vagy ha a randomGeneration false-ra van állítva, 
+ *akkor a megadott parancsra) új vonatot generál, 
+ *és elindítja a hozzá kapcsolt BoardElement felé.
+ */
 public class EntryPoint extends BoardElement implements Updateable{
 	
+	/**
+	 * A belépési pont számlálója, ha eléri a 0 értéket új vonatot küld a pályára
+	 */
 	private int time;
+	/**
+	 * Default várakozási idõ, ha az újonnan létrehozott vonat elhagyja a belépési pontot, 
+	 * erre az értékre ugrik vissza a számláló
+	 */
 	private int defWaitTime;
+	/**
+	 * referencia a Timer objektumra, hogy az új vonatok beregisztrálhatók
+	 */
 	private Timer timer;
+	/**
+	 * ettõl függõen generál a belépési pont random paraméterekkel vonatot, 
+	 * ha hamis, megadott értékek alapján teszi ezt.
+	 */
 	private boolean randomGeneration = false;
+	/**
+	 * trainModellek listája
+	 */
 	private List<TrainModel> trainModelList = new ArrayList<TrainModel>();
 	
+	/**
+	 * Wagon súlya
+	 */
 	private final int WAGONWEIGHT = 10000;
+	/**
+	 * Mozdony súlya
+	 */
 	private final int LOCOMOTIVEWEIGHT = 20000;
+	/**
+	 * Mozdony erõ
+	 */
 	private final int LOCOMOTIVEPOWER = 10000000;
+	/**
+	 * utas szám
+	 */
 	private final int PASSENGERCOUNT = 50;
+	/**
+	 * maximum wagonok száma
+	 */
 	private final int MAXWAGONNUMBER = 6;
+	/**
+	 * speciális wagon esélye
+	 */
 	private final double SPECIALWAGONCHANCE = 0.3;
+	/**
+	 * LoweWagon esélye
+	 */
 	private final double LOVEWAGONCHANCE = 0.5;
 	
+	/**
+	 * @param defWaitTime
+	 * @param length
+	 * @param t
+	 * , beállítja a default várakozási idõt, és kap egy referenciát a Timer-hez, 
+	 * hogy beregisztrálhassa az újonnan létrejövõ vonatokat.
+	 */
 	public EntryPoint(int defWaitTime, int length, Timer t){
 		super(length);
 		this.defWaitTime = defWaitTime;
@@ -38,6 +88,14 @@ public class EntryPoint extends BoardElement implements Updateable{
 		t.registerElement(this);
 	}
 	
+	/**
+	 * @param c
+	 * @param passengerCount
+	 * @param wagonType
+	 * @return
+	 * egy vagon létrehozásáért felelõs, 
+	 * teszteléshez, manuális validáláshoz szükséges.
+	 */
 	private Wagon createWagon(int c, int passengerCount, String wagonType){
 		Wagon w = null;
 		
@@ -53,6 +111,10 @@ public class EntryPoint extends BoardElement implements Updateable{
 		return w;
 	}
 	
+	/**
+	 * @param tm
+	 * tm vonat hozzáadása trainModel listához
+	 */
 	public void addTrain(TrainModel tm){
 		trainModelList.add(tm);
 	}
@@ -61,6 +123,11 @@ public class EntryPoint extends BoardElement implements Updateable{
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	
+	/**
+	 * @return
+	 * @throws EndGameException
+	 * random wagonok létrehozása
+	 */
 	private Wagon createWagonRandom() throws EndGameException{
 		Wagon w;
 		double specialWagonChance = Math.random();
@@ -82,6 +149,11 @@ public class EntryPoint extends BoardElement implements Updateable{
 		return w;
 	}
 	
+	/**
+	 * @return
+	 * @throws EndGameException
+	 * készít egy vagon listát, a megadott számnak megfelelõen, random értékekkel.
+	 */
 	private List<Wagon> createWagonList() throws EndGameException{
 		
 		int wagonNumber =(int)((Math.random()*10)%MAXWAGONNUMBER);
@@ -93,6 +165,10 @@ public class EntryPoint extends BoardElement implements Updateable{
 		return wagonsList;
 	}
 	
+	/**
+	 * @throws EndGameException
+	 * vonat létrehozása
+	 */
 	public void createTrainRandom() throws EndGameException{
 
 		List<Wagon> wagonList = createWagonList();
@@ -106,12 +182,19 @@ public class EntryPoint extends BoardElement implements Updateable{
 	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	
+	/**
+	 * Újraindítja a saját idõzítõjét
+	 */
 	public void resetTimer(){
 
 		time = defWaitTime;
 
 	}
 	
+	/**
+	 * @throws EndGameException
+	 * Ha nincs engedélyezve a random, akkor pályára engedi a vonatot
+	 */
 	public void getNextTrain() throws EndGameException{
 		List<Wagon> wagonList = new ArrayList<Wagon>();
 		for (int i=0; i<trainModelList.get(0).countOfWagons; i++){
@@ -124,6 +207,11 @@ public class EntryPoint extends BoardElement implements Updateable{
 		trainModelList.remove(0);
 	}
 	
+	/* (non-Javadoc)
+	 * @see update.Updateable#update()
+	 * A számlálójának függvényében cselekszik (csökkenti a számlálót, 
+	 * új vonatot hoz létre, ha van új vonat, de még nem hagyta el, akkor vár)
+	 */
 	public void update() throws EndGameException{
 		
 		if (this.randomGeneration == false) return;
@@ -137,16 +225,28 @@ public class EntryPoint extends BoardElement implements Updateable{
 		//System.out.println("update returned");
 	}
 	
+	/* (non-Javadoc)
+	 * @see boardelements.BoardElement#enter(trainelements.Train)
+	 * lekezeli a vonat érkezését
+	 */
 	public void enter(Train t) throws EndGameException{
 
 		if (next.isOccupied() == true) throw new EndGameException(); 
 		
 	}
 	
+	/**
+	 * @param randomGeneration
+	 * beállítja a random generátor értékét
+	 */
 	public void setRandomGeneration(boolean randomGeneration) {
 		this.randomGeneration = randomGeneration;
 	}
 	
+	/* (non-Javadoc)
+	 * @see boardelements.BoardElement#getNext()
+	 * visszaadja a következõ pályaelemet
+	 */
 	@Override
 	public BoardElement getNext() throws EndGameException{
 		
@@ -155,6 +255,10 @@ public class EntryPoint extends BoardElement implements Updateable{
 		return next;
 	}
 
+	/* (non-Javadoc)
+	 * @see boardelements.BoardElement#toString()
+	 * kiirás teszteléshez
+	 */
 	@Override
 	public String toString(){
 		return this.getId();
