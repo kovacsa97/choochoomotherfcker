9f7286b76c0868c2211c5e8d5949d0dd415399c5
szoke <szoke@152.66.183.9>
2017-04-13 23:18:59 +0200

    CrossingRail comment

--- boardelements/CrossingRail.java
+++ boardelements/CrossingRail.java
@@ -1,23 +1,47 @@
 package boardelements;
 
+/**
+ * olyan sínpárt reprezentál, amit egy másik (hasonló osztályú) sínpár keresztez, 
+ * és kezeli a keresztezõdésben való ütközést.
+ *
+ */
 public class CrossingRail extends Rail{
 	
+	/**
+	 * keresztezõ sínt tárolja
+	 */
 	private Rail otherRail;
 
+	/**
+	 * @param length
+	 * Rail konstruktorát meghívja lengt hosszal
+	 */
 	public CrossingRail(int length) {
 		super(length);
 	}
 	
+	/**
+	 * @param r
+	 * beállítja az õt keresztezõ sínt.
+	 */
 	public void setOtherRail(Rail r){
 		otherRail = r;
 	}
 	
+	/* (non-Javadoc)
+	 * @see boardelements.BoardElement#lock()
+	 * a másik sínt is lockolja.
+	 */
 	@Override
 	public void lock(){
 		this.lock();
 		otherRail.lock();
 	}
 	
+	/* (non-Javadoc)
+	 * @see boardelements.BoardElement#unlock()
+	 * másik sínt is feloldja.
+	 */
 	@Override
 	public void unlock(){
 		this.unlock();
