386e1e39c5365afa0436a85302d7704d1631d2f4
szoke <szoke@152.66.183.9>
2017-04-13 23:21:21 +0200

    Rail comment

--- boardelements/Rail.java
+++ boardelements/Rail.java
@@ -1,7 +1,15 @@
 package boardelements;
 
+/**
+ * Egy egyszerû sínpárt valósít meg. 
+ *
+ */
 public class Rail extends BoardElement{
 
+	/**
+	 * @param length
+	 * BoardElement kontruktorát meghívja length hosszal.
+	 */
 	public Rail(int length) {
 		super(length);
 		//System.out.println("New Rail created");
