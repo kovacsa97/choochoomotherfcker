5ff2fe409090dd6c70df0729247ad226a7607aa2
MateSzoke <szokemate@yahoo.com>
2017-04-14 00:12:11 +0200

    Comment Train

--- trainelements/Train.java
+++ trainelements/Train.java
@@ -9,21 +9,53 @@ import position.*;
 import update.Timer;
 import update.Updateable;
 
+/**
+ * A vonatot reprezentáló osztály
+ */
 public class Train implements Updateable {
+	/**
+	 * A vonathoz tartozó vagonok listája
+	 */
 	private List<Wagon> myWagons;
+	/**
+	 * A vonathoz tartozó mozdony
+	 */
 	private Locomotive myLocomotive;
+	/**
+	 * A vonat elejét reprezentáló paraméter
+	 */
 	private StartPos startPos;
+	/**
+	 * A vonat végét reprezentáló paraméter
+	 */
 	private EndPos endPos;
+	/**
+	 * Vonat id-ja
+	 */
 	private String id;
 	
+	/**
+	 * @param wagons
+	 * @param loc
+	 * @param ep
+	 * @throws EndGameException
+	 * vagonokat, egy mozdonyt kap, 
+	 * valamint a kezdő pályaelemét, mellyel a kező- és végpozícióját állítja be
+	 */
 	public Train(List<Wagon> wagons, Locomotive loc, EntryPoint ep) throws EndGameException{
-		System.out.println("New Train created with parameters of type List<Wagon>, " + loc.toString() + " " + ep.toString());
+		//System.out.println("New Train created with parameters of type List<Wagon>, " + loc.toString() + " " + ep.toString());
 		myLocomotive = loc;
 		myWagons = wagons;
 		startPos = new StartPos(ep, this);
 		endPos = new EndPos(ep, startPos, 0);
 	}
 	
+	/**
+	 * @param c
+	 * @return
+	 * utasokat leszállító függvény, egy színt kap, 
+	 * mely alapján kiválasztja a megfelelő vagont, melyről leszállhatnak az utasok
+	 */
 	public int getPassengers(Color c){
 		int i = 0;
 		while (i!=myWagons.size()){
@@ -38,7 +70,14 @@ public class Train implements Updateable {
 		return 0;
 	}
 	
+	/**
+	 * @param c
+	 * @param n
+	 * Utasokat felszállító függvény, mely megkapja egy egész számban, 
+	 * hogy hány utas száll fel, valamint egy színt, hogy melyik vonatra szállhatnak fel
+	 */
 	public void passengerGetOn(Color c, int n){
+		//System.out.println("getPassengers was called inside class Train with parameters of type Color");
 		int i = 0;
 		while (i!=myWagons.size()){
 			if (myWagons.get(i).getColor() == c){
@@ -50,6 +89,10 @@ public class Train implements Updateable {
 		}
 	}
 
+	/* (non-Javadoc)
+	 * @see update.Updateable#update()
+	 * A vonat változásait kezelő függvény: mozgás és utaspontszám ciklusonkénti változását kezeli.
+	 */
 	@Override
 	public void update() throws EndGameException {
 		int dist = myLocomotive.getExcursion();
@@ -60,14 +103,22 @@ public class Train implements Updateable {
 		}
 	}
 	
+	/**
+	 * @return a vonat id-je
+	 */
 	public String getId() {
 		return id;
 	}
 
+	/**
+	 * @param id
+	 * Vonat id-jét beállítja
+	 */
 	public void setId(String id) {
 		this.id = id;
 	}
 
+	//TODO
 	@Override
 	public String toString(){
 		return "Train";
