a7702c1b1530dc98eac38c71ffc326fe5d1805be
szoke <szoke@152.66.183.9>
2017-04-13 23:26:55 +0200

    Switch comment

--- boardelements/Switch.java
+++ boardelements/Switch.java
@@ -2,22 +2,44 @@ package boardelements;
 
 import main.EndGameException;
 
+/**
+ *Váltó, a hozzá csatlakozó két BoardElement közül az egyik felé továbbítja 
+ *a rá érkezõ vonatot
+ *
+ */
 public class Switch extends BoardElement{
 	
+	/**
+	 * Pályaelem, melyre eltérítheti a vonatot az eredeti útvonal helyett
+	 */
 	private BoardElement nextSecond;
+	/**
+	 * a vonat irányát határozza meg a két kijárat közül.
+	 */
 	private boolean nextActive;
 
+	/**
+	 * @param length
+	 * konstruktor, length hosszal létrehoz egy váltót
+	 */
 	public Switch(int length) {
 		super(length);
-		System.out.println("New Switch created with parameters of int");
+		//System.out.println("New Switch created with parameters of int");
 		nextActive = true;
 
 	}
 	
+	/**
+	 * @param b
+	 * váltóhoz az eredeti útvonalhoz képest másik, b kijáratot adó metódus
+	 */
 	public void addExit(BoardElement b){
 		nextSecond = b  ;
 	}
 	
+	/**
+	 * váltó irányát változtatja
+	 */
 	public void changeDir(){
 		if (nextActive == true)
 			nextActive = false;
@@ -25,6 +47,10 @@ public class Switch extends BoardElement{
 			nextActive = true;
 	}
 	
+	/* (non-Javadoc)
+	 * @see boardelements.BoardElement#getNext()
+	 * Visszaadja a next elemet
+	 */
 	@Override
 	public BoardElement getNext() throws EndGameException{
 	 if (prev.isOccupied() == false) return prev;
@@ -34,6 +60,10 @@ public class Switch extends BoardElement{
 	 else return nextSecond;
 	}
 	
+	/* (non-Javadoc)
+	 * @see boardelements.BoardElement#toString()
+	 * Kiirás teszthez
+	 */
 	@Override
 	public String toString(){
 		return super.toString() + " " + nextSecond.getId();
