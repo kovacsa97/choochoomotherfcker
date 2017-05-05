5e9af6b5aec46bdf7d742cdcedb4c86de237fea4
kovacsa97 <adamkovacs.liverpool@gmail.com>
2017-05-05 17:15:17 +0200

    station, rail

--- visuals/RailVisual.java
+++ visuals/RailVisual.java
@@ -9,7 +9,16 @@ public class RailVisual extends StaticVisual{
 	}
 
 	public void draw(GraphicsContext c) {
-		// TODO Auto-generated method stub
+		int x1 = this.startPos.x;
+		int y1 = this.startPos.y;
+		int x2 = this.endPos.x;
+		int y2 = this.endPos.y;
 		
+		int a = x2 - x1;
+		int b = y2 - y1;
+		
+		int d = (int)Math.sqrt(a*a+b*b);
+		c.rotate(90);
+		c.fillRect(x1, y1, d, 30);
 	}
 }
