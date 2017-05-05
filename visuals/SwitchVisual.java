366e6f939696c6ee0cd219739407f445b19bc9a0
kovacsa97 <adamkovacs.liverpool@gmail.com>
2017-05-06 00:50:41 +0200

    switchVisual

--- visuals/SwitchVisual.java
+++ visuals/SwitchVisual.java
@@ -1,8 +1,10 @@
 package visuals;
 
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.transform.Affine;
 import javafx.scene.transform.Rotate;
 import javafx.util.Builder;
 import sun.java2d.loops.FillParallelogram;
@@ -17,7 +19,7 @@ public class SwitchVisual extends DynamicVisual {
 
 	@Override
 	public void draw(GraphicsContext c) {
-		Polygon p = new Polygon();
+		/*Polygon p = new Polygon();
 		p.getPoints().addAll(new Double[]{ (double) 100, (double) 100, (double) 200, (double) 200, (double) 200, (double) 230, (double) 100, (double) 130});
 		p.getTransforms().add(new Rotate(70, 0, 0));
 		p.getLocalToSceneTransform();
@@ -34,6 +36,74 @@ public class SwitchVisual extends DynamicVisual {
 		for(int i = 0; i < 4; i++){
 			System.out.println(x[i] + " " + y[i]);
 		}
-		c.fillPolygon(x, y, 4);
+		c.fillPolygon(x, y, 4);*/
+		
+		int x1 = this.startPos.x;
+		int y1 = this.startPos.y;
+		int x2 = this.endPos.x;
+		int y2 = this.endPos.y;
+		
+		int a = x2 - x1;
+		int b = y2 - y1;
+		
+		boolean isNegative = false;
+		
+		if (a<0){
+			a*=-1;
+			isNegative = true;
+		}
+		if (b<0){
+			b*=-1;
+			isNegative = true;
+		}
+		
+		int d = (int)Math.sqrt(a*a+b*b);
+
+		double sina = (double) b / (double)d;
+		double alpha;
+		if (isNegative){
+			alpha = Math.asin(sina) * -57;
+		}
+		else{
+			alpha = Math.asin(sina) * 57;
+		}
+		
+		c.save();
+		c.transform(new Affine(new Rotate(alpha, x1, y1+5)));	    
+		c.setFill(Color.GRAY);
+	    c.fillRect(x1, y1, d, 10);
+	    c.restore();
+
+		x2 = this.otherEnd.x;
+		y2 = this.otherEnd.y;
+		
+		a = x2 - x1;
+		b = y2 - y1;
+		
+		isNegative = false;
+		
+		if (a<0){
+			a*=-1;
+			isNegative = true;
+		}
+		if (b<0){
+			b*=-1;
+			isNegative = true;
+		}
+		
+		d = (int)Math.sqrt(a*a+b*b);
+		sina = (double) b / (double)d;
+		if (isNegative){
+			alpha = Math.asin(sina) * -57;
+		}
+		else{
+			alpha = Math.asin(sina) * 57;
+		}
+		
+		c.save();
+		c.transform(new Affine(new Rotate(alpha, x1, y1+5)));
+	    c.setFill(Color.GRAY);
+	    c.fillRect(x1, y1, d, 10);
+	    c.restore();
 	}
 }
