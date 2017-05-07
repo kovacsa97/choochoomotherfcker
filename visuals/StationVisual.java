ca05b4e540a6a0e0206a200b50cc4e7beabf6c6c
kovacsa97 <adamkovacs.liverpool@gmail.com>
2017-05-07 23:51:39 +0200

    stationVisual correction

--- visuals/StationVisual.java
+++ visuals/StationVisual.java
@@ -22,17 +22,20 @@ public class StationVisual extends StaticVisual{
 		int x2 = this.endPos.x;
 		int y2 = this.endPos.y;
 		
+		if (endPos.x-startPos.x<0){
+			 x1 = this.endPos.x;
+			 y1 = this.endPos.y;
+			 x2 = this.startPos.x;
+			 y2 = this.startPos.y;
+		}
+		
 		int a = x2 - x1;
 		int b = y2 - y1;
 		
 		
 		boolean isNegative = false;
 		boolean otherSide = false;
-		
-		if (a<0){
-			a*=-1;
-			otherSide = true;
-		}
+
 		if (b<0){
 			b*=-1;
 			isNegative = true;
