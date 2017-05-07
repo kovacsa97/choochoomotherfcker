0f0c69209ec212428e9e4a94e3e6938d736b7f27
matesebi <matesebi96@gmail.com>
2017-05-08 00:00:41 +0200

    main changed

--- main/Main.java
+++ main/Main.java
@@ -1,7 +1,12 @@
 package main;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
 
 import controller.Controller;
 import javafx.application.Application;
@@ -10,11 +15,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import view.View;
-import visuals.DynamicVisual;
-import visuals.Point;
-import visuals.StaticVisual;
-import visuals.StationVisual;
-import visuals.SwitchVisual;
+import visuals.*;
+//import visuals.Point;
+//import visuals.StaticVisual;
+//import visuals.StationVisual;
+//import visuals.SwitchVisual;
 
 
 public class Main extends Application {
@@ -23,15 +28,17 @@ public class Main extends Application {
 	
 	
 	public void test() {
-		ArrayList<StaticVisual> l=new ArrayList<StaticVisual>();
-		l.add(new StationVisual(new Point(10,20), new Point(120,130), "fuck_u"));
-		w.initBoard(l);
+//		ArrayList<StaticVisual> l=new ArrayList<StaticVisual>();
+//		//l.add(new StationVisual(new Point(10,200), new Point(70,200), "fuck_u"));
+//		w.initBoard(l);
+//		
+//		ArrayList<DynamicVisual> dl=new ArrayList<DynamicVisual>();
+//		dl.add(new TunnelOpportunityVisual(new Point(40,200), new Point(700,150), "TheUltimateSwitch"));
+//		w.update(dl);
 		
-		ArrayList<DynamicVisual> dl=new ArrayList<DynamicVisual>();
-		dl.add(new SwitchVisual(new Point(100,130), new Point(130,100), new Point(130,160), "TheUltimateSwitch"));
-		w.update(dl);
-		
-		c.updateInfo();
+			c.displayBoard("assets/map_1_visual.xml");
+			c.displayChange();
+			//c.updateInfo();
 	}
 	
 	
