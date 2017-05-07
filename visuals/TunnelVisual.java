36ab68ab08eadb309580cb87d810e1d09c7e275a
kovacsa97 <adamkovacs.liverpool@gmail.com>
2017-05-07 22:28:11 +0200

    tunnelVisual

--- visuals/TunnelVisual.java
+++ visuals/TunnelVisual.java
@@ -1,6 +1,9 @@
 package visuals;
 
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.transform.Affine;
+import javafx.scene.transform.Rotate;
 
 public class TunnelVisual extends DynamicVisual {
 
@@ -10,8 +13,41 @@ public class TunnelVisual extends DynamicVisual {
 
 	@Override
 	public void draw(GraphicsContext c) {
-		// TODO Auto-generated method stub
+		int x1 = this.startPos.x;
+		int y1 = this.startPos.y;
+		int x2 = this.endPos.x;
+		int y2 = this.endPos.y;
 		
+		int a = x2 - x1;
+		int b = y2 - y1;
+		
+		boolean isNegative = false;
+		
+		if (a<0){
+			a*=-1;
+			isNegative = true;
+		}
+		if (b<0){
+			b*=-1;
+			isNegative = true;
+		}
+		
+		int d = (int)Math.sqrt(a*a+b*b);
+
+		double sina = (double) b / (double)d;
+		double alpha;
+		if (isNegative){
+			alpha = Math.asin(sina) * -57;
+		}
+		else{
+			alpha = Math.asin(sina) * 57;
+		}
+		
+		c.save();
+	    c.transform(new Affine(new Rotate(alpha, x1, y1+5)));
+	    c.setFill(Color.BROWN);
+	    c.fillRect(x1, y1, d, 20);
+	    c.restore();
 	}
 
 }
