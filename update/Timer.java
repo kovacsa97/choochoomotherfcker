14c3bff6392948fcdfc26e3bf04158361e35b3ae
Lorinc Kiss <kissslorinc@hotmail.com>
2017-05-08 12:58:47 +0200

    added game over dialog

--- update/Timer.java
+++ update/Timer.java
@@ -1,12 +1,17 @@
 package update;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import javax.swing.JFrame;
 
 import controller.Controller;
 import javafx.application.Platform;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ChoiceDialog;
 import main.EndGameException;
 import trainelements.Train;
 
@@ -58,10 +63,15 @@ public class Timer extends Thread{
 				Thread.sleep(300);
 			}
 		} catch (EndGameException e){
-			if (e.getMessage() == "Game Over"){
-				System.out.println(e.getStackTrace() + " GAME OVER");
+				Platform.runLater(()->{
+					Alert alert = new Alert(AlertType.ERROR);
+					alert.setTitle("Game over");
+					alert.setHeaderText("The game is over.");
+					alert.setContentText("Two of your trains crashed!");
+
+					alert.showAndWait();
+				});
 				stopMe();
-			}
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
